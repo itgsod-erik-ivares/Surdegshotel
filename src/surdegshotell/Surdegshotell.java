31fbe985c00041afbee1456a9d99cd96d36f6608
Erik <Erik@193.11.10.88>
2015-01-13 09:42:44 +0100

    testing åäö

--- src/surdegshotell/Surdegshotell.java
+++ src/surdegshotell/Surdegshotell.java
@@ -9,6 +9,6 @@ public class Surdegshotell {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here 
+        // TODO code application logic here
     }
 }
