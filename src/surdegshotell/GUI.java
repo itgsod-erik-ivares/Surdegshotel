16218c561b3b182b0107edebfc539217db4dda08
Erik <Erik@193.11.10.88>
2015-01-19 14:12:32 +0100

    added GUI for mending and statistics tabb.

--- src/surdegshotell/GUI.java
+++ src/surdegshotell/GUI.java
@@ -1,11 +1,7 @@
 package surdegshotell;
 
 import java.awt.BorderLayout;
-import java.awt.CardLayout;
 import java.awt.Dimension;
-import java.awt.LayoutManager;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.util.ArrayList;
 import javax.swing.DefaultListModel;
 import javax.swing.JButton;
@@ -15,30 +11,41 @@ import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTabbedPane;
 import javax.swing.JTextField;
-import javax.swing.plaf.basic.BasicTabbedPaneUI;
 
 /**
  *
  * @author Erik
  */
 public class GUI extends JFrame {
-    private JTabbedPane panel = new JTabbedPane();
-    private JPanel _checkInOut = new JPanel(new BorderLayout());
-    private JPanel _mending = new JPanel(new BorderLayout());
-    private JPanel _statistics = new JPanel(new BorderLayout());
+    private final JTabbedPane panel = new JTabbedPane();
+    private final JPanel _checkInOut = new JPanel(new BorderLayout());
+    private final JPanel _mending = new JPanel(new BorderLayout());
+    private final JPanel _statistics = new JPanel(new BorderLayout());
     
-    private DefaultListModel model = new DefaultListModel();
-    private JList _checkInList = new JList(model); 
-    private JScrollPane scrollbar = new JScrollPane(_checkInList);
+    private final DefaultListModel _checkInListModel = new DefaultListModel();
+    private final JList _checkInList = new JList(_checkInListModel); 
+    private final JScrollPane _checkInScrollbar = new JScrollPane(_checkInList);
     private ArrayList<JTextField> _checkInFields = new ArrayList();
-    private JButton _checkInButton = new JButton("Check In");
-    private JButton _checkOutButton = new JButton("Check Out");
+    private final JButton _checkInButton = new JButton("Check In");
+    private final JButton _checkOutButton = new JButton("Check Out");
+    
+    private final DefaultListModel _mendingListModel = new DefaultListModel();
+    private final JList _mendingList = new JList(_mendingListModel);
+    private final JScrollPane _mendingScrollbar = new JScrollPane(_mendingList);
+    private final JButton _mendingDoneButton = new JButton("Done");
+    
+    private final JTextField _statisticsFrom = new JTextField("From");
+    private final JTextField _statisticsTo = new JTextField("to");
+    private final JTextField _priceTextField = new JTextField("price");
+    private final JButton _priceSubmittButton = new JButton("Submitt");
+    private final JButton _statisticsGetButton = new JButton("Get Statistics");
     
     public GUI(){
         setTitle("Sourdough hotel");
         fillGUI();
         pack();
         setVisible(true);
+        setLocationRelativeTo(null);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
     }
     
@@ -58,7 +65,7 @@ public class GUI extends JFrame {
             _checkInFields.add(new JTextField("Testing"));
         }
         _checkInList.setPreferredSize(new Dimension(350, 250));
-        scrollbar.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
+        _checkInScrollbar.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
         _checkInOut.add(_checkInList, BorderLayout.NORTH);
         JPanel checkInCenterPanel = new JPanel();
         _checkInOut.add(checkInCenterPanel, BorderLayout.CENTER);
@@ -71,10 +78,26 @@ public class GUI extends JFrame {
     }
 
     private void createMending() {
-
+        _mendingList.setPreferredSize(new Dimension(350, 250));
+        _mendingScrollbar.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
+        _mending.add(_mendingList, BorderLayout.NORTH);
+        _mending.add(_mendingDoneButton, BorderLayout.SOUTH);
     }
 
     private void createStatistics() {
-
+        _statisticsFrom.setPreferredSize(new Dimension(100, 25));
+        _statisticsTo.setPreferredSize(new Dimension(100, 25));
+        _priceTextField.setPreferredSize(new Dimension(100, 25));
+        _priceSubmittButton.setPreferredSize(new Dimension(75, 50));
+        _statisticsGetButton.setPreferredSize(new Dimension(150, 50));
+        JPanel centerStatisticsPanel = new JPanel();
+        JPanel southStatisticsPanel = new JPanel();
+        _statistics.add(centerStatisticsPanel, BorderLayout.CENTER);
+        _statistics.add(southStatisticsPanel, BorderLayout.SOUTH);
+        centerStatisticsPanel.add(_statisticsFrom);
+        centerStatisticsPanel.add(_statisticsTo);
+        centerStatisticsPanel.add(_statisticsGetButton);
+        southStatisticsPanel.add(_priceTextField, BorderLayout.SOUTH);
+        southStatisticsPanel.add(_priceSubmittButton, BorderLayout.SOUTH);
     }
 }
