e3c7873468999d517df6823393b915bd056f48a3
Erik <Erik@193.11.10.88>
2015-01-21 12:01:15 +0100

    created convert to string method and finiched it

--- src/surdegshotell/StringHandler.java
+++ src/surdegshotell/StringHandler.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package surdegshotell;
 
 /**
@@ -10,5 +5,12 @@ package surdegshotell;
  * @author Erik
  */
 public class StringHandler {
-    
+    public static String convertToString(String[] stringList){
+        StringBuilder returnString = new StringBuilder();
+        for (String string : stringList) {
+            returnString.append(string+";");
+        }
+        returnString.deleteCharAt(returnString.length()-1);
+        return returnString.toString();
+    }
 }
