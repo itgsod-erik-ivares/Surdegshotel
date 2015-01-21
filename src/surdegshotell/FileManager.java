e75d7fbff51e762e0acf48bc57487d6a8a3abb12
Erik <Erik@193.11.10.88>
2015-01-21 12:01:33 +0100

    created write to file methid and finished it

--- src/surdegshotell/FileManager.java
+++ src/surdegshotell/FileManager.java
@@ -1,14 +1,24 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package surdegshotell;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Writer;
 /**
  *
  * @author Erik
  */
 public class FileManager {
-    
+    public static void writeToFile(String file, String line){
+        BufferedWriter output;
+        try{
+            output = new BufferedWriter(new FileWriter(file, true));  //clears file every time
+            output.append(line);
+            output.newLine();
+            output.close();
+        }
+        catch(IOException ioe){
+            System.out.println("Chould not find file!!!");
+        }
+    }
 }
