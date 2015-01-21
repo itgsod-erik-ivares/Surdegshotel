44a26e5b31ccab6782b74bd9054e0329c6ca7557
Erik <Erik@193.11.10.88>
2015-01-21 12:01:47 +0100

    finiched validator class

--- src/surdegshotell/Validator.java
+++ src/surdegshotell/Validator.java
@@ -1,14 +1,59 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package surdegshotell;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  *
  * @author Erik
  */
 public class Validator {
+    public static boolean validateInt(String integer){
+        boolean returnValue = false;
+        
+        try{
+            Integer.parseInt(integer);
+            System.out.println("int: OK");
+            returnValue = true;
+        }
+        catch(NumberFormatException nfe){
+            System.out.println("int: fail");
+            returnValue = false;
+        }
+      return returnValue;
+    }
     
+    public static boolean validatePhoneNumber(String phoneNumber){
+        boolean returnValue = false;
+        try{
+            Integer.parseInt(phoneNumber);
+            if (phoneNumber.length() > 5 && phoneNumber.length() < 14) {
+                System.out.println("number: OK");
+                returnValue = true;
+            }
+        }
+        catch(NumberFormatException nfe){
+            System.out.println("number: fail");
+            returnValue = false;
+        }
+        
+        return returnValue;
+    }
+    
+    public static boolean validateEMail(String email){
+        System.out.println(email);
+        boolean returnValue = false;
+        Pattern emailPattern;
+        emailPattern = Pattern.compile("^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,6}$", Pattern.CASE_INSENSITIVE);
+        Matcher matcher;
+        matcher = emailPattern.matcher(email);
+        if (matcher.matches()) {
+            System.out.println("email: OK");
+            returnValue = true;
+        }
+        else{
+            System.out.println("email: fail");
+        }
+        return returnValue;
+    }
 }
