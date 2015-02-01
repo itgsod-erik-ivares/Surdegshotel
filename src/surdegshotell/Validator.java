047384796a459df83121f183f1f81b23361c5b56
Erik <Erik@Erik-PC>
2015-02-01 20:51:08 +0100

    added/fixed comments

--- src/surdegshotell/Validator.java
+++ src/surdegshotell/Validator.java
@@ -4,10 +4,17 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 /**
- *
+ * a class used to validate different types of data
  * @author Erik
+ * @version 2015-02-01
  */
 public class Validator {
+    
+    /**
+     * validates if a string is parseble into an int
+     * @param integer the string that reresent an integer
+     * @return true if the string was parseble
+     */
     public static boolean validateInt(String integer){
         boolean returnValue = false;
         
@@ -23,6 +30,11 @@ public class Validator {
       return returnValue;
     }
     
+    /**
+     * validates if a String is a valid phonenumber
+     * @param phoneNumber the string to be validated
+     * @return true if the string was valid
+     */
     public static boolean validatePhoneNumber(String phoneNumber){
         boolean returnValue = false;
         try{
@@ -40,6 +52,11 @@ public class Validator {
         return returnValue;
     }
     
+    /**
+     * validates a string to se if it was a valid email address
+     * @param email the string to be validated
+     * @return true if the string was a valid email
+     */
     public static boolean validateEMail(String email){
         System.out.println(email);
         boolean returnValue = false;
