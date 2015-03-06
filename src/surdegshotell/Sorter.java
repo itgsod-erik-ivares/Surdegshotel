f58e974ee9b01b4e97909368062e1619c5287eec
Erik <Erik@193.11.11.201>
2015-03-06 08:29:20 +0100

    cleaned up code

--- src/surdegshotell/Sorter.java
+++ src/surdegshotell/Sorter.java
@@ -35,25 +35,17 @@ public class Sorter {
         for (String aDough : checkedInDoughs) {
             String[] dough = aDough.split(";");
             String temp = dough[10];
-            System.out.println(dough[10]);
             Date date = new Date(Long.parseLong(temp));
-            System.out.println(formatter.format(date) + " <-- Date test");
             Calendar checkInDate = Calendar.getInstance();
             checkInDate.setTime(date);
-            System.out.println(checkInDate.getTime());
             while(checkInDate.before(today)){
-                System.out.println("test3");
                 int counter = 0;
                 String regString = formatter.format(checkInDate.getTime());
-                System.out.println(regString);
                 String todString = formatter.format(today.getTime());
-                System.out.println(todString);
                 if(regString.equals(todString)){
-                    System.out.println("test4");
                     String countString = Integer.toString(counter);
                     StringHandler.add(aDough, countString);
                     returnList.add(aDough);
-                    System.out.println("dough added to mending");
                     checkInDate.add(Calendar.DAY_OF_MONTH, Integer.parseInt(dough[5]));
                 }
                 else{
@@ -71,30 +63,25 @@ public class Sorter {
      * @param to the date you whant the methosd to stop sorting at
      * @return ArratList<String> a list with the doughs
      */
-    public static ArrayList<String> getDoughsFromTo(String from, String to){
+    public static ArrayList<String> getDoughsFromTo(String from, String to) throws ParseException {
         ArrayList<String> returnList = new ArrayList<>();
         ArrayList<String> checkedOutDoughs = FileManager.readFile("CheckedOut.txt");
-        try{
-            Date dateTo = formatter.parse(to);
-            Calendar calTo = Calendar.getInstance();
-            calTo.setTime(dateTo);
-            Date dateFrom = formatter.parse(from);
-            Calendar calFrom = Calendar.getInstance();
-            calFrom.setTime(dateFrom);
-            calTo.add(Calendar.DAY_OF_MONTH, 1);
-            calFrom.add(Calendar.DAY_OF_MONTH, -1);
-            for (String adough : checkedOutDoughs) {
-                String[] dough = adough.split(";");
-                Date date = new Date(Long.parseLong(dough[11]));
-                Calendar checkOut = Calendar.getInstance();
-                checkOut.setTime(date);
-                if (checkOut.before(calTo) && checkOut.after(calFrom)) {
-                    returnList.add(adough);
-                }
+        Date dateTo = formatter.parse(to);
+        Calendar calTo = Calendar.getInstance();
+        calTo.setTime(dateTo);
+        Date dateFrom = formatter.parse(from);
+        Calendar calFrom = Calendar.getInstance();
+        calFrom.setTime(dateFrom);
+        calTo.add(Calendar.DAY_OF_MONTH, 1);
+        calFrom.add(Calendar.DAY_OF_MONTH, -1);
+        for (String adough : checkedOutDoughs) {
+            String[] dough = adough.split(";");
+            Date date = new Date(Long.parseLong(dough[11]));
+            Calendar checkOut = Calendar.getInstance();
+            checkOut.setTime(date);
+            if (checkOut.before(calTo) && checkOut.after(calFrom)) {
+                returnList.add(adough);
             }
-        }
-        catch(ParseException pe){
-            System.out.println("Could not parse date when filtering");
         }
         return returnList;
     }
@@ -110,10 +97,7 @@ public class Sorter {
         for (String cod : checkedOutDoughs) {
             String[] codList = cod.split(";");
             String[] dList = dough.split(";");
-            System.out.println("Dough ID: " + dList[0]);
-            System.out.println("Currents search item: "+ codList[0]);
             if (codList[0].equals(dList[0])) {
-                System.out.println("Item found!");
                 Date ci = new Date(Long.parseLong(dList[10]));
                 Calendar cci = Calendar.getInstance();
                 cci.setTime(ci);
@@ -122,14 +106,12 @@ public class Sorter {
                 Date co = new Date(Long.parseLong(codList[11]));
                 Calendar cco = Calendar.getInstance();
                 cco.setTime(co);
-                System.out.println("Checking mendings: ");
                 int mendCounter = 0;
                 while(ccit.before(cco)){
                     mendCounter++;
                     ccit.add(Calendar.DAY_OF_MONTH, Integer.parseInt(codList[5]));
                 }
                 int daycounter = 0;
-                System.out.println("Checking days");
                 while (cci.before(cco)) {
                     daycounter++;
                     cci.add(Calendar.DAY_OF_MONTH, 1);
@@ -138,9 +120,6 @@ public class Sorter {
                 returnList.add(Integer.toString(cost));
                 returnList.add(Integer.toString(mendCounter));
             }
-        }
-        for (String info : returnList) {
-            System.out.println(info);
         }
         return returnList;
     }
